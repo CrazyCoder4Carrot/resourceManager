d6496e898f27a17f164e85277a88aee61fa42a0f
unknown <liuzhenb@LIUZHENB-MOBL.ccr.corp.intel.com>
2015-04-22 15:16:34 +0800

    upload

--- app.js
+++ app.js
@@ -33,7 +33,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-
 app.use('/', login);
 app.use('/users', users);
 app.use('/login',login);
--- routes/order.js
+++ routes/order.js
@@ -1,8 +1,11 @@
 var db = require('../lib/database');
 var express = require('express');
 var moment = require('moment');
+var fs = require('fs');
+var formidable = require("formidable");
 var router = express.Router();
-
+var AVATAR_UPLOAD_FOLDER = '/avatar/';
+var TITLE = 'formidable上传示例';
 /* GET users listing. */
 router.get('/', function(req, res, next)
 {
@@ -17,7 +20,7 @@ router.get('/', function(req, res, next)
                 res.render('order',
                 {
                     allOrderInfo: queryResults,
-                     moment: moment
+                    moment: moment
                 });
             }
             else
@@ -64,14 +67,14 @@ router.post('/add', function(req, res, next)
     if (sess.results)
     {
         var orderInfo = {
-            category:req.body.category,
-            vendor:req.body.vendor,
-            quantity:req.body.quantity,
-            unitPrice:req.body.unitPrice,
-            order:req.body.order,
-            date:req.body.date,
-            comment:req.body.comment,
-            team:req.body.team,
+            category: req.body.category,
+            vendor: req.body.vendor,
+            quantity: req.body.quantity,
+            unitPrice: req.body.unitPrice,
+            order: req.body.order,
+            date: req.body.date,
+            comment: req.body.comment,
+            team: req.body.team,
         }
         db.addOrder(orderInfo, function(err, queryResults)
         {
@@ -98,16 +101,16 @@ router.post('/update', function(req, res, next)
     {
 
         var orderInfo = {
-            id:req.body.id,
-            category:req.body.category,
-            vendor:req.body.vendor,
-            quantity:req.body.quantity,
-            unitPrice:req.body.unitPrice,
-            order:req.body.order,
-            date:req.body.date,
-            comment:req.body.comment,
-            team:req.body.team,
-            id:req.body.id
+            id: req.body.id,
+            category: req.body.category,
+            vendor: req.body.vendor,
+            quantity: req.body.quantity,
+            unitPrice: req.body.unitPrice,
+            order: req.body.order,
+            date: req.body.date,
+            comment: req.body.comment,
+            team: req.body.team,
+            id: req.body.id
         }
         db.updateOrder(orderInfo, function(err, queryResults)
         {
@@ -126,6 +129,68 @@ router.post('/update', function(req, res, next)
         res.redirect('/login');
     }
 
+});
+
+router.post('/file-upload', function(req, res, next)
+{
+    var form = new formidable.IncomingForm();
+    form.encoding = 'utf-8';
+    form.uploadDir = 'public' + AVATAR_UPLOAD_FOLDER;
+    form.keepExtensions = true;
+    form.maxFieldsSize = 2 * 1024 * 1024;
+
+    form.parse(req, function(err, fields, files)
+    {
+
+        if (err)
+        {
+            res.locals.error = err;
+            res.render('index',
+            {
+                title: TITLE
+            });
+            return;
+        }
+        console.log("file tpye "+ files.fulAvatar.type);
+        var extName = ''; //后缀名
+        switch (files.fulAvatar.type)
+        {
+            case 'image/pjpeg':
+                extName = 'jpg';
+                break;
+            case 'image/jpeg':
+                extName = 'jpg';
+                break;
+            case 'image/png':
+                extName = 'png';
+                break;
+            case 'image/x-png':
+                extName = 'png';
+                break;
+        }
+
+        if (extName.length == 0)
+        {
+            res.locals.error = '只支持png和jpg格式图片';
+            res.render('index',
+            {
+                title: TITLE
+            });
+            return;
+        }
+
+        var avatarName = Math.random() + '.' + extName;
+        var newPath = form.uploadDir + avatarName;
+
+        console.log(newPath);
+        fs.renameSync(files.fulAvatar.path, newPath); //重命名
+    });
+
+    res.locals.success = '上传成功';
+    res.render('index',
+    {
+        title: TITLE
+    });
 });
 
 module.exports = router;
